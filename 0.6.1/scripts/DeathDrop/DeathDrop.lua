1317eb4ef27b2cb3ef7f453cd06aad11630bef8a
Michael Fitzmayer <mail@michael-fitzmayer.de>
2017-12-03 19:44:22 +0100

    Removed trailing whitespaces. Adjusted intendation.

--- 0.6.1/scripts/DeathDrop/DeathDrop.lua
+++ 0.6.1/scripts/DeathDrop/DeathDrop.lua
@@ -15,45 +15,47 @@ Methods = {}
 -- [ DeathDrop.Drop(pid) ]
 -- directly above it.
 
-Methods.Drop = function(pid)	
-	for index, item in pairs(Players[pid].data.inventory) do
-	
-		local mpNum = WorldInstance:GetCurrentMpNum() + 1
-		local cell = tes3mp.GetCell(pid)
-		local location = {
-			posX = tes3mp.GetPosX(pid), posY = tes3mp.GetPosY(pid), posZ = tes3mp.GetPosZ(pid),
-		}
-		local refId = item.refId
-		local refIndex =  0 .. "-" .. mpNum
-		
-		WorldInstance:SetCurrentMpNum(mpNum)
-		tes3mp.SetCurrentMpNum(mpNum)
 
-		LoadedCells[cell]:InitializeObjectData(refIndex, refId)
-		LoadedCells[cell].data.objectData[refIndex].location = location
-		table.insert(LoadedCells[cell].data.packets.place, refIndex)
-		LoadedCells[cell]:Save()
-		
-		for onlinePid, player in pairs(Players) do
-			if player:IsLoggedIn() then
-				tes3mp.InitializeEvent(onlinePid)
-				tes3mp.SetEventCell(cell)
-				tes3mp.SetObjectRefId(refId)
-				tes3mp.SetObjectRefNumIndex(0)
-				tes3mp.SetObjectMpNum(mpNum)
+Methods.Drop = function(pid)
+    for index, item in pairs(Players[pid].data.inventory) do
 
-				tes3mp.SetObjectPosition(location.posX, location.posY, location.posZ)
-				tes3mp.AddWorldObject()
-				tes3mp.SendObjectPlace()
-			end
-		end
-		
-		Players[pid].data.inventory[index] = nil
-	end
-	
-	Players[pid]:Save()
+        local mpNum = WorldInstance:GetCurrentMpNum() + 1
+        local cell = tes3mp.GetCell(pid)
+        local location = {
+            posX = tes3mp.GetPosX(pid), posY = tes3mp.GetPosY(pid), posZ = tes3mp.GetPosZ(pid),
+        }
+        local refId = item.refId
+        local refIndex =  0 .. "-" .. mpNum
+
+        WorldInstance:SetCurrentMpNum(mpNum)
+        tes3mp.SetCurrentMpNum(mpNum)
+
+        LoadedCells[cell]:InitializeObjectData(refIndex, refId)
+        LoadedCells[cell].data.objectData[refIndex].location = location
+        table.insert(LoadedCells[cell].data.packets.place, refIndex)
+        LoadedCells[cell]:Save()
+
+        for onlinePid, player in pairs(Players) do
+            if player:IsLoggedIn() then
+                tes3mp.InitializeEvent(onlinePid)
+                tes3mp.SetEventCell(cell)
+                tes3mp.SetObjectRefId(refId)
+                tes3mp.SetObjectRefNumIndex(0)
+                tes3mp.SetObjectMpNum(mpNum)
+
+                tes3mp.SetObjectPosition(location.posX, location.posY, location.posZ)
+                tes3mp.AddWorldObject()
+                tes3mp.SendObjectPlace()
+            end
+        end
+
+        Players[pid].data.inventory[index] = nil
+    end
+
+    Players[pid]:Save()
     Players[pid]:LoadInventory()
-	Players[pid]:LoadEquipment()
+    Players[pid]:LoadEquipment()
 end
+
 
 return Methods
