e447b8a42684b0e9573bfa80723651bce66a30e9
Michael Fitzmayer <mail@michael-fitzmayer.de>
2017-12-12 14:08:29 +0100

    Add check to ignore translation of unknown languages

--- modules/BabelFish/main.lua
+++ modules/BabelFish/main.lua
@@ -130,8 +130,8 @@ Event.register(Events.ON_PLAYER_SENDMESSAGE, function(player, message, channel)
                                    end
                                end
 
-                               if translations[receiverLang] == nil then
-                                   translations[receiverLang] =  message
+                               if translations[receiverLang] == nil or string.match(translations[receiverLang], "{ Error: The language") then
+                                   translations[receiverLang] = message
                                end
                            end
 
