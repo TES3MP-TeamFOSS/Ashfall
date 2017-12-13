07ff9af5edcf419dfbd4e6c7bdb826582694d6d5
1338 <maxvandersluis@yahoo.com>
2017-12-13 22:45:04 +0100

    Woohoo. We rolling our own google translate API on nodeJS. Like true webscale warriors.

---
+++ web/BabelFish/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const translate = require('google-translate-api');
+var app = express();
+port = 8000;
+
+app.listen(port);
+app.get('/', function(req, res) {
+  var translateObj = {};
+  if(req.query.to){
+    translateObj['to'] = req.query.to;
+  }
+  if(req.query.from){
+    translateObj['from'] = req.query.from;
+  }
+  translate(req.query.text, translateObj).then(result => {
+    res.send(result.text);
+  }).catch(err => {
+    console.error(err);
+  });
+});
